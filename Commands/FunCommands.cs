c9da7bb89a2c0af3b6dd44c70260c3b42bfc3801
iamprod <50503822+iamprod@users.noreply.github.com>
2020-03-09 15:21:05 -0500

    added xtylerd command

--- Commands/FunCommands.cs
+++ Commands/FunCommands.cs
@@ -15,6 +15,12 @@ namespace DiscordBotHavi.Commands
         public async Task Ping(CommandContext ctx)
         {
             await ctx.Channel.SendMessageAsync(ctx.User.Mention +  " pong").ConfigureAwait(false);
-        }  
+        }
+        [Command("xtylerd")]
+        [DSharpPlus.CommandsNext.Attributes.Description("Flames Tyler.")]
+        public async Task FlameTyler(CommandContext ctx)
+         u r xd
+            await ctx.Channel.SendMessageAsync("@tyrapple#5036 u r xd").ConfigureAwait(false);
+        }
     }
 }
